69ec7b95b13addf17767f0951d7ad371a82ac2b7
SehyeonKang <gedfgrdt@naver.com>
2023-06-28 23:27:45 +0900

    Feat: 회원가입 폼, 화면 추가

--- src/main/java/com/hkcommunity/infra/Config/SecurityConfig.java
+++ src/main/java/com/hkcommunity/infra/Config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.hkcommunity.infra.Config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +22,11 @@ public class SecurityConfig {
                 .anyRequest().authenticated();
 
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
