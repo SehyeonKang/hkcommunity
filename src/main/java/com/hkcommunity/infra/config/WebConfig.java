1edc060e86727c58c51749369d2373a5bcb0ff90
SehyeonKang <gedfgrdt@naver.com>
2023-08-02 21:06:20 +0900

    Feat: 알림 아이콘 추가, 알림 생성시 아이콘 색 변경 기능 추가
    리다이렉트 요청과 static 리소스 요청시에는 적용하지 않도록 함

--- src/main/java/com/hkcommunity/infra/config/WebConfig.java
+++ src/main/java/com/hkcommunity/infra/config/WebConfig.java
@@ -1,19 +1,39 @@
 package com.hkcommunity.infra.config;
 
+import com.hkcommunity.modules.notification.NotificationInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.StaticResourceLocation;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final NotificationInterceptor notificationInterceptor;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         PageableHandlerMethodArgumentResolver pageableHandlerMethodArgumentResolver = new PageableHandlerMethodArgumentResolver();
         pageableHandlerMethodArgumentResolver.setMaxPageSize(3);
         argumentResolvers.add(pageableHandlerMethodArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        List<String> staticResourcesPath = Arrays.stream(StaticResourceLocation.values())
+                .flatMap(StaticResourceLocation::getPatterns)
+                .collect(Collectors.toList());
+        staticResourcesPath.add("/node_modules/**");
+
+        registry.addInterceptor(notificationInterceptor)
+                .excludePathPatterns(staticResourcesPath);
     }
 }
--- src/main/java/com/hkcommunity/modules/notification/NotificationRepository.java
+++ src/main/java/com/hkcommunity/modules/notification/NotificationRepository.java
@@ -1,6 +1,9 @@
 package com.hkcommunity.modules.notification;
 
+import com.hkcommunity.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    long countByAccountAndChecked(Account account, boolean checked);
 }
